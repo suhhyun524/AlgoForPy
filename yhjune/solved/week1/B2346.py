290e1b6e9e136f0fec11bb5e917233836623f2bc
HyoJung Yoon <yhjune09@gmail.com>
2023-08-17 23:47:15 +0900

    🔒 B2346 / 풍선 터뜨리기 / 실버2

--- yhjune/solved/week1/B2346.py
+++ yhjune/solved/week1/B2346.py
@@ -1,19 +1,30 @@
 # https://www.acmicpc.net/problem/2346 풍선 터뜨리기
-# 
+
+# 덱, 
+# 로테이트
 
 import sys
 import collections
 input = sys.stdin.readline
 bal = dict()
 deque = collections.deque
-start = 1
+target, count = 1
 
 n = int(input().split())
 list = list(map(int, input().split()))
 
-for i in range(n):
-    bal.update({i:list[i]})
-    deque.append(list[i])
+for i in range(1, n+1):
+    bal.update({i:list[i-1]})
+    deque.append(list[i-1])
 
-while start <= n :
+print(deque[-2])
+
+# while count <= n :
+#     print(target)
+#     a = bal.get(target)
+#     if a < 0 :
+#         deque.pop
     
+#     target = 
+#     count +=1
+
